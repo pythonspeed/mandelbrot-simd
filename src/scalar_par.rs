4528cd73e8e681c51ca2588741bf138f2176b206
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-09-26 10:05:30 -0400

    Simplify the core logic.

--- src/scalar_par.rs
+++ src/scalar_par.rs
@@ -10,64 +10,22 @@ struct Complex {
     imag: f64,
 }
 
-#[inline]
-fn diverged(c: Complex) -> bool {
-    let rr = c.real * c.real;
-    let ii = c.imag * c.imag;
-
-    rr + ii > THRESHOLD
-}
-
-
-/// An iterator yielding the infinite Mandelbrot sequence
-struct MandelbrotIter {
-    /// Initial value which generated this sequence
-    start: Complex,
-    /// Current iteration value
-    current: Complex,
-}
-
-impl MandelbrotIter {
-    /// Creates a new Mandelbrot sequence iterator for a given starting point
-    fn new(start: Complex) -> Self {
-        Self { start, current: start }
-    }
-
-    /// Returns the number of iterations it takes for the Mandelbrot sequence
-    /// to diverge at this point, or `ITER_LIMIT` if it doesn't diverge.
-    fn count(mut self) -> u32 {
-        let mut z = self.start;
-        for i in 0..ITER_LIMIT {
-            if diverged(z) {
-                return i;
-            }
-
-            z = self.next().unwrap();
+/// Returns the number of iterations it takes for the Mandelbrot sequence
+/// to diverge at this point, or `ITER_LIMIT` if it doesn't diverge.
+fn get_count(start: Complex) -> u32 {
+    let mut current = start.clone();
+    for iteration in 0..ITER_LIMIT {
+        let rr = current.real.powi(2);
+        let ii = current.imag.powi(2);
+        if rr + ii > THRESHOLD {
+            return iteration;
         }
-        ITER_LIMIT
+        let ri = current.real * current.imag;
+
+        current.real = start.real + (rr - ii);
+        current.imag = start.imag + (ri + ri);
     }
-}
-
-impl Iterator for MandelbrotIter {
-    type Item = Complex;
-
-    /// Generates the next value in the sequence
-    #[inline]
-    fn next(&mut self) -> Option<Complex> {
-        let Complex { real: c_x, imag: c_y } = self.start;
-        let Complex { real: x, imag: y } = self.current;
-
-        let xx = x * x;
-        let yy = y * y;
-        let xy = x * y;
-
-        let new_x = c_x + (xx - yy);
-        let new_y = c_y + (xy + xy);
-
-        self.current = Complex { real: new_x, imag: new_y };
-
-        Some(self.current)
-    }
+    ITER_LIMIT
 }
 
 pub fn generate(dims: Dimensions, xr: Range, yr: Range) -> Vec<u32> {
@@ -99,7 +57,7 @@ pub fn generate(dims: Dimensions, xr: Range, yr: Range) -> Vec<u32> {
         row.iter_mut().enumerate().for_each(|(j, count)| {
             let x = xs[j];
             let z = Complex { real: x, imag: y };
-            *count = MandelbrotIter::new(z).count() as u32;
+            *count = get_count(z) as u32;
         });
     });
 
