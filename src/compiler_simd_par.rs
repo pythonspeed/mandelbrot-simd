868f5906dd63709496a9c53633c3dda3aafde1e9
Giovanni Cuccu <gcuccu@imolainformatica.it>
2024-10-13 18:08:00 +0200

    parallel version

--- src/compiler_simd_par.rs
+++ src/compiler_simd_par.rs
@@ -19,13 +19,13 @@ fn set_count(start: [Complex;BATCH_SIZE], counts:&mut[u32]) {
     let mut rr=[0_f64;BATCH_SIZE];
     let mut ii=[0_f64;BATCH_SIZE];
     let mut ri=[0_f64;BATCH_SIZE];
+    assert_eq!(counts.len(), BATCH_SIZE);
     for i in 0..BATCH_SIZE {
         counts[i]=ITER_LIMIT;
     }
     for iteration in 0..ITER_LIMIT {
         for i in 0..BATCH_SIZE {
             if counts[i]==ITER_LIMIT {
-            //if rr[i] + ii[i] <= THRESHOLD {
                 rr[i] = current[i].real * current[i].real;
                 ii[i] = current[i].imag * current[i].imag;
                 if rr[i] + ii[i] > THRESHOLD {
@@ -49,10 +49,10 @@ pub fn generate(dims: Dimensions, xr: Range, yr: Range) -> Vec<u32> {
 
         let mut buf:Vec<f64> = Vec::new();
 
-        buf=(0..width)
-            .into_iter()
+        (0..width)
+            .into_par_iter()
             .map(|j| xr.start + dx * (j as f64))
-            .collect();
+            .collect_into_vec(&mut buf);
 
         buf
     };
@@ -61,13 +61,12 @@ pub fn generate(dims: Dimensions, xr: Range, yr: Range) -> Vec<u32> {
 
     let len = width * height;
     let mut out = Vec::with_capacity(len);
-    //let mut out = vec![ITER_LIMIT;len];
     unsafe {
         out.set_len(len);
     }
 
 
-    out.chunks_mut(width).enumerate().for_each(|(i, row)| {
+    out.par_chunks_mut(width).enumerate().for_each(|(i, row)| {
         let y = yr.start + dy * (i as f64);
         let mut zs=[Complex{real:0_f64,imag:0_f64};BATCH_SIZE];
         row.chunks_exact_mut(BATCH_SIZE).enumerate().for_each(|(j, count)| {
@@ -75,8 +74,6 @@ pub fn generate(dims: Dimensions, xr: Range, yr: Range) -> Vec<u32> {
                 zs[i].real=xs[j*BATCH_SIZE+i];
                 zs[i].imag=y;
             }
-            //let x = xs[j];
-            //let z = Complex { real: x, imag: y };
             set_count(zs,count);
         });
     });
